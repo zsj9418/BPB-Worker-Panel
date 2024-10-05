45f89b286e64961fbcf9c51643780f736cfb054b
zsj9418 <34057614+zsj9418@users.noreply.github.com>
2024-10-05 09:46:37 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = 'c0d6af72-9e76-4048-bcdc-b856772bd131';
+let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
