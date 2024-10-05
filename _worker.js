c3bd80061436d62505b95a5ac910284b0987d7de
zsj9418 <34057614+zsj9418@users.noreply.github.com>
2024-10-05 09:35:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'c0d6af72-9e76-4048-bcdc-b856772bd131';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
@@ -286,8 +286,8 @@ export default {
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.speedtest.net';
+                         return new Response('Not found', { status: 404 });
+                        url.hostname = 'www.baidu.com';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
