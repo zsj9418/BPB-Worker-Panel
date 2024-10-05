3e796c4b0c89c84a4f551ec4446b510e49a57e6f
zsj9418 <34057614+zsj9418@users.noreply.github.com>
2024-10-05 09:54:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'c2c8a201-09b9-47a7-b336-fcf1657f54a8';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
@@ -286,8 +286,8 @@ export default {
                         });
 
                     default:
-                        return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.fenxm.com';
+                        // return new Response('Not found', { status: 404 });
+                        url.hostname = 'www.visa.com';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
